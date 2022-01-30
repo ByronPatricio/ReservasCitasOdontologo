917770587daa186721fac40b86fef338337db527
byron.cueva.e@gmail.com <byron.cueva.e@gmail.com>
2022-01-30 14:37:59 -0500

    V01: HASTA PANTALLA PRINCIPAL CON ACCESO A LOS DIFERENTES ROLES DE LA APLICACIÓN

---
+++ Vistas/modulos/seleccionar.php
@@ -0,0 +1,75 @@
+<section class="content">
+
+  <center>
+    <h1>Sistema de Reservas de Citas para el Consultorio Dental</h1>
+    <h1> </h1>
+    <h1>SELECCIONE SU ROL DE ACCESO</h1>
+  </center>
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box" style="background-color:#8A2BE2; color: white;">
+            <div class="inner">
+              <h2>Gestión</h2> <p><h2>Administrativa</h2>
+
+              <p>Inicie Sesión</p>
+            </div>
+            <div class="icon">
+              
+            </div>
+            <a href="ingreso-Secretaria" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box" style="background-color: #B22222; color: white;">
+            <div class="inner">
+            <h2>Gestión De</h2> <p><h2>Odontólogos</h2>
+
+              <p>Inicie Sesión</p>
+            </div>
+            <div class="icon">
+              
+            </div>
+            <a href="ingreso-Doctor" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+            <h2>Gestión De</h2> <p><h2>Reservas</h2>
+
+              <p>Inicie Sesión</p>
+            </div>
+            <div class="icon">
+            
+            </div>
+            <a href="ingreso-Paciente" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box"  style="background-color: #808080; color: white">
+            <div class="inner">
+            <h2>Administrador Del</h2> <p><h2>Sistema</h2>
+
+              <p>Inicie Sesión</p>
+            </div>
+            <div class="icon">
+              
+            </div>
+            <a href="ingreso-Administrador" class="small-box-footer">Ingresar <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+    
+
+    </section>
---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php
+   
+   
+        require_once("Controladores/plantillaC.php");
+        $plantilla = new Plantilla();
+        $plantilla->LlamarPlantilla();;
+    
+?>
